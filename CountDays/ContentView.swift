28c954292ee67bb16a59ce6876e308e3a44664cb
citylighxts <hanazizahn@gmail.com>
2024-02-20 01:49:23 +0700

    Initial Commit

---
+++ CountDays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountDays
+//
+//  Created by Hana Azizah on 20/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountDays/CountDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDaysApp.swift
+//  CountDays
+//
+//  Created by Hana Azizah on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CountDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
